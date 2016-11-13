054c1d03684f6f6fa38970592648f6bf2da5acb6
Daniel James <daniel.james@scorebig.com>
2016-11-13 03:13:33 -0800

    Hid Facebook share for now 

--- src/Share.js
+++ src/Share.js
@@ -2,9 +2,9 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
 export default class Share extends React.Component {
-	render() {
-		let location = encodeURIComponent(window.location);
-		let message = encodeURIComponent(this.props.message);
+	renderFacebookShare(location) {
+		// HACK: need to work out UX for posting to Facebook since pre-filled messages can't be used anymore
+		return;
 
 		const post = () => {
 			if (!this.props.enabled) return;
@@ -12,24 +12,42 @@ export default class Share extends React.Component {
 			window.open(`http://www.facebook.com/sharer.php?u=${location}`, '_blank');
 		}
 
-		const tweet = () => {
+		return (
+			<div>
+				<a href="#" onClick={post}>
+					Review and Post
+					<FontAwesome name="facebook" size="2x" />
+				</a>
+			</div>
+		);
+	}
+
+	renderTwitterShare(location, message) {
+				const tweet = () => {
 			if (!this.props.enabled) return;
 
 			window.open(`https://twitter.com/share?url=${location}&text=${message}`, '_blank');
 		}
 
-	  return (
-	    <div>
-	    	<a href="#" onClick={post}>
-	    		Review and Post
-	    		<FontAwesome name="facebook" size="2x" />
-	    	</a>
-	    	<a href="#" onClick={tweet}>
-	    		Review and Tweet
-	    		<FontAwesome name="twitter" size="2x" />
-	    	</a>
-	    </div>
-	  );
+		return (
+				<div>
+			  	<a href="#" onClick={tweet}>
+			  		Review and Tweet
+			  		<FontAwesome name="twitter" size="2x" />
+			  	</a>
+			  </div>
+		);
+	}
+	render() {
+		let location = encodeURIComponent(window.location);
+		let message = encodeURIComponent(this.props.message);
+
+		return (
+			<div>
+			  {this.renderFacebookShare(location)}
+			  {this.renderTwitterShare(location, message)}
+			</div>
+		);
 	}
 }
 
